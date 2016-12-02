9be66709e328d1314d3d416aa348de9a87b117f6
iyer18 <iyer18@purdue.edu>
2016-12-01 20:02:03 -0500

    followed tutorial to create user authentication

--- app.js
+++ app.js
@@ -7,24 +7,53 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+// create a new express server
+var app = express();
+
 var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 //var mongodbURL = '';
 var mongodbURL = 'mongodb://localhost/db';
 mongoose.connect(mongodbURL);
+
+require('./config/passport')(passport); // pass passport for configuration
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
-// create a new express server
-var app = express();
+
+
+//app.configure(function() {
+
+	// set up our express application
+	app.use(morgan('dev')); // log every request to the console
+	app.use(cookieParser()); // read cookies (needed for auth)
+	app.use(bodyParser()); // get information from html forms
+
+	app.set('view engine', 'ejs'); // set up ejs for templating
+
+	// required for passport
+	app.use(session({ secret: 'rajgunjanrajgunjan' })); // session secret
+	app.use(passport.initialize());
+	app.use(passport.session()); // persistent login sessions
+	app.use(flash()); // use connect-flash for flash messages stored in session
+
+//});
 
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+
+
+require('./app/routes.js')(app, passport);
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
